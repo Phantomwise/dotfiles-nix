a469bae2a5b62b9173324748a6c2a39fbd9d7fe9
Phantomwise <phantomwise67@protonmail.com>
2025-06-19 02:12:20 +0200

    🔧 Add XDG path variables

--- nix/etc/nixos/configuration.nix
+++ nix/etc/nixos/configuration.nix
@@ -156,9 +156,8 @@
 	environment.sessionVariables = rec {
 		XDG_CACHE_HOME  = "$HOME/.cache";
 		XDG_CONFIG_HOME = "$HOME/.config";
-		# XDG_DATA_HOME   = "$HOME/.local/share";
-		# XDG_STATE_HOME  = "$HOME/.local/state";
-
+		XDG_DATA_HOME   = "$HOME/.local/share";
+		XDG_STATE_HOME  = "$HOME/.local/state";
 		# Not officially in the specification
 		# XDG_BIN_HOME    = "$HOME/.local/bin";
 		# PATH = [ 
