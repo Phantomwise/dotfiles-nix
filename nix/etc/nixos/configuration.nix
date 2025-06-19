43cf760c6c9ca43dab606ba09b13c3e1790115d2
Phantomwise <phantomwise67@protonmail.com>
2025-06-19 03:14:32 +0200

    ♻️ Split environment variables from configuration.nix to environment.nix

--- nix/etc/nixos/configuration.nix
+++ nix/etc/nixos/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./environment.nix
       ./fonts.nix
     ];
 
@@ -136,22 +137,6 @@
 	services.upower.enable = true;
 
 	users.defaultUserShell = pkgs.zsh;
-
-	environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-	# This is using a rec (recursive) expression to set and access XDG_BIN_HOME within the expression
-	# For more on rec expressions see https://nix.dev/tutorials/first-steps/nix-language#recursive-attribute-set-rec
-	environment.sessionVariables = rec {
-		XDG_CACHE_HOME  = "$HOME/.cache";
-		XDG_CONFIG_HOME = "$HOME/.config";
-		XDG_DATA_HOME   = "$HOME/.local/share";
-		XDG_STATE_HOME  = "$HOME/.local/state";
-		# Not officially in the specification
-		# XDG_BIN_HOME    = "$HOME/.local/bin";
-		# PATH = [ 
-			# "${XDG_BIN_HOME}"
-		# ];
-	};
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
