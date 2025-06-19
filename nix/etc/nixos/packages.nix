820d8e6044750384e01420551331867c00cddc41
Phantomwise <phantomwise67@protonmail.com>
2025-06-19 12:50:51 +0200

    ♻️ Split packages config from configuration.nix to packages.nix

---
+++ nix/etc/nixos/packages.nix
@@ -0,0 +1,78 @@
+{ config, pkgs, ... }:
+
+{
+
+	# Allow unfree packages
+	nixpkgs.config.allowUnfree = true;
+
+	# List packages installed in system profile. To search, run:
+	# $ nix search wget
+	environment.systemPackages = with pkgs; [
+		cron
+		git
+		gitmoji-cli
+		gvfs
+		gnome.gvfs
+		killall
+		inotify-tools
+		steam-run
+		stow
+		wl-clipboard
+		# zsh
+		zsh-autocomplete
+		zsh-autosuggestions
+		#  wget
+		### COMPOSITOR ###
+		dunst
+		(rofi-wayland.override { plugins = [ pkgs.rofi-calc pkgs.rofi-games ]; })
+		rofimoji
+		# waybar
+		nemo-with-extensions
+		nemo-fileroller
+		### TERMINAL ###
+		foot
+		kitty
+		### EDITORS ###
+		featherpad
+		kakoune
+		micro
+		# leafpad # error: 'leafpad' has been removed due to lack of maintenance upstream. Consider using 'xfce.mousepad' instead
+		# ms-edit
+		# vscode-with-extensions
+		vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+		### MISC ###
+		discord-canary
+		keepassxc
+		libation
+		synology-drive-client
+
+			(vscode-with-extensions.override {
+				vscodeExtensions = with vscode-extensions; [
+					bbenoist.nix
+					ms-python.python
+					ms-azuretools.vscode-docker
+					ms-vscode-remote.remote-ssh
+				] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+					{
+						name = "remote-ssh-edit";
+						publisher = "ms-vscode-remote";
+						version = "0.47.2";
+						sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+					}
+				];
+			})
+
+		vscode-extensions.seatonjiang.gitmoji-vscode
+		];
+
+	programs.sway.enable = true;
+	programs.waybar.enable = true;
+
+	programs.firefox.enable = true;
+
+	programs.zsh.enable = true;
+	programs.zsh.enableCompletion = true;
+	programs.zsh.autosuggestions.enable = true;
+	programs.zsh.syntaxHighlighting.enable = true;
+
+}
