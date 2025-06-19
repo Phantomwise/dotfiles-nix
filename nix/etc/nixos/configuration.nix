077de0063255c825c99d3b67fb1e8933bb938af2
Phantomwise <phantomwise67@protonmail.com>
2025-06-19 22:47:13 +0200

    ⚗️ Trying to make portals work :/

--- nix/etc/nixos/configuration.nix
+++ nix/etc/nixos/configuration.nix
@@ -67,6 +67,17 @@
 
 	users.defaultUserShell = pkgs.zsh;
 
+	xdg.portal = {
+		enable = true;
+		wlr.enable = true;
+		# gtk portal needed to make firefox happy
+		extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+		# gtkUsePortal = true;
+			# error:
+			# The option definition `xdg.portal.gtkUsePortal' in `/home/phantomwise/SynologyDrive/dotfiles-nix/nix/etc/nixos/configuration.nix' no longer has any effect; please remove it.
+			# This option has been removed due to being unsupported and discouraged by the GTK developers.
+	};
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
