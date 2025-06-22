1321439e8aee9d53897bd3ed306459c24e8d3acd
Phantomwise <phantomwise67@protonmail.com>
2025-06-22 16:39:31 +0200

    ✨ Add packages: Emulators

--- nix/etc/nixos/packages-games.nix
+++ nix/etc/nixos/packages-games.nix
@@ -3,6 +3,21 @@
 {
 
 	environment.systemPackages = with pkgs; [
+
+		### EMULATORS ###
+		dosbox # DOS Emulator
+		mgba # GBA Emulator
+		vbam # GBA Emulator
+		desmume # NDS Emulator
+		melonDS # NDS Emulator
+		cemu # Wii U Emulator
+		dolphin-emu # Gamecube/Wii/Triforce Emulator for x86_64 and ARMv8
+		# dolphin-emu-primehack
+
+		### STORES ###
 		lutris
+
+		### GAMES ###
+
 	];
 }
