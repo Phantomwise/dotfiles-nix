7fd853317a3e4ff67c53bb3df0ff3ff41349c77b
Phantomwise <phantomwise67@protonmail.com>
2025-07-05 18:39:49 +0200

    🎨 Replace spaces by tabs

--- nix/etc/nixos/configuration.nix
+++ nix/etc/nixos/configuration.nix
@@ -5,62 +5,62 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./environment.nix
-      ./fonts.nix
-      ./packages.nix
-      ./packages-games.nix
-    ];
+	imports =
+		[ # Include the results of the hardware scan.
+			./hardware-configuration.nix
+			./environment.nix
+			./fonts.nix
+			./packages.nix
+			./packages-games.nix
+		];
 
 	# nixpkgs.overlays = [
 		# (import ./overlays/libation-overlay.nix)
 	# ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+	# Bootloader.
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
 
-  # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+	# Use latest kernel.
+	boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+	networking.hostName = "nixos"; # Define your hostname.
+	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+	# Configure network proxy if necessary
+	# networking.proxy.default = "http://user:password@proxy:port/";
+	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+	# Enable networking
+	networking.networkmanager.enable = true;
 
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+	hardware.bluetooth.enable = true; # enables support for Bluetooth
+	hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
-  # Set your time zone.
-  time.timeZone = "Europe/Paris";
+	# Set your time zone.
+	time.timeZone = "Europe/Paris";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+	# Select internationalisation properties.
+	i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "fr_FR.UTF-8";
-    LC_IDENTIFICATION = "fr_FR.UTF-8";
-    LC_MEASUREMENT = "fr_FR.UTF-8";
-    LC_MONETARY = "fr_FR.UTF-8";
-    LC_NAME = "fr_FR.UTF-8";
-    LC_NUMERIC = "fr_FR.UTF-8";
-    LC_PAPER = "fr_FR.UTF-8";
-    LC_TELEPHONE = "fr_FR.UTF-8";
-    LC_TIME = "fr_FR.UTF-8";
-  };
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "fr_FR.UTF-8";
+		LC_IDENTIFICATION = "fr_FR.UTF-8";
+		LC_MEASUREMENT = "fr_FR.UTF-8";
+		LC_MONETARY = "fr_FR.UTF-8";
+		LC_NAME = "fr_FR.UTF-8";
+		LC_NUMERIC = "fr_FR.UTF-8";
+		LC_PAPER = "fr_FR.UTF-8";
+		LC_TELEPHONE = "fr_FR.UTF-8";
+		LC_TIME = "fr_FR.UTF-8";
+	};
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "colemak";
-  };
+	# Configure keymap in X11
+	services.xserver.xkb = {
+		layout = "us";
+		variant = "colemak";
+	};
 
 	# Define a user account. Don't forget to set a password with ‘passwd’.
 	users.users.phantomwise = {
@@ -88,31 +88,31 @@
 			# This option has been removed due to being unsupported and discouraged by the GTK developers.
 	};
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+	# Some programs need SUID wrappers, can be configured further or are
+	# started in user sessions.
+	# programs.mtr.enable = true;
+	# programs.gnupg.agent = {
+	#	 enable = true;
+	#	 enableSSHSupport = true;
+	# };
 
-  # List services that you want to enable:
+	# List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+	# Enable the OpenSSH daemon.
+	# services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+	# Open ports in the firewall.
+	# networking.firewall.allowedTCPPorts = [ ... ];
+	# networking.firewall.allowedUDPPorts = [ ... ];
+	# Or disable the firewall altogether.
+	# networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+	# This value determines the NixOS release from which the default
+	# settings for stateful data, like file locations and database versions
+	# on your system were taken. It‘s perfectly fine and recommended to leave
+	# this value at the release version of the first install of this system.
+	# Before changing this value read the documentation for this option
+	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+	system.stateVersion = "25.05"; # Did you read the comment?
 
 }
