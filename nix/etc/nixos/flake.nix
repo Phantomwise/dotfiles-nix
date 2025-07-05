56afd9b8319b33553ab303a619e529775df83452
Phantomwise <phantomwise67@protonmail.com>
2025-07-05 17:30:26 +0200

    🎨 Change spaces to tabs

--- nix/etc/nixos/flake.nix
+++ nix/etc/nixos/flake.nix
@@ -1,22 +1,21 @@
 {
-  description = "Override libation to build from latest git";
+	description = "Override libation to build from latest git";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
-    libation_src = {
-      url = "https://github.com/rmcrackan/Libation";
-      flake = false;
-    };
-  };
+		libation_src = {
+			url = "https://github.com/rmcrackan/Libation";
+			flake = false;
+		};
+	};
 
-  outputs = { self, nixpkgs, libation_src }: let
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
-  in {
-    defaultPackage.x86_64-linux = pkgs.libation.overrideAttrs (oldAttrs: {
-      src = libation_src;
-      version = "nightly-${libation_src.shortRev or "src"}";
-    });
-  };
+	outputs = { self, nixpkgs, libation_src }: let
+		pkgs = import nixpkgs { system = "x86_64-linux"; };
+	in {
+		defaultPackage.x86_64-linux = pkgs.libation.overrideAttrs (oldAttrs: {
+			src = libation_src;
+			version = "nightly-${libation_src.shortRev or "src"}";
+		});
+	};
 }
-
