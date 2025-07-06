423d09c203c1cd02042d0dbac7fe347b73182482
Phantomwise <phantomwise67@protonmail.com>
2025-07-06 10:36:15 +0200

    🎨 Split Makefile: put nix-update in a separate file

---
+++ nix/nix-update.mk
@@ -0,0 +1,9 @@
+# Declare phony targets
+.PHONY: nix-update
+
+# nix-update
+nix-update:
+	@echo -e "\033[1;33mUpdating channels\033[0m"
+	nix-channel --update
+	@echo -e "\033[1;33mRebuilding\033[0m"
+	sudo nixos-rebuild switch
