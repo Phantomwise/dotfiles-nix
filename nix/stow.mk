2c8b70cae4d4cb8e495a0686ab74b7fb213672c1
Phantomwise <phantomwise67@protonmail.com>
2025-07-14 15:24:41 +0200

    🔧 Update unstow tasks to follow restow tasks

--- nix/stow.mk
+++ nix/stow.mk
@@ -39,20 +39,31 @@ endif
 
 # unstow
 unstow:
-	@echo -e "\033[1;33mDeleteing ${TARGET}\033[0m"
+	@echo -e "\033[1;33mDeleting ${TARGET}\033[0m"
 ifeq ($(TARGET),all)
 	stow -v --delete --dir=$(CONFIG_DIR) home --target=$(HOME_DIR)
 	sudo stow -v --delete --dir=$(CONFIG_DIR) etc --target=/etc
 	sudo stow -v --delete --dir=$(CONFIG_DIR) usr --target=/usr
-	stow -v --delete --dir=$(PERSONAL_DIR) . --target=$(HOME_DIR)
+	stow -v --delete --dir=$(PERSONAL_DIR) home --target=$(HOME_DIR)
+	stow -v --delete --dir=$(PERSONAL_DIR) home-adopt --target=$(HOME_DIR)
 else ifeq ($(TARGET),home)
+	# Restow dotfiles-nix/home
 	stow -v --delete --dir=$(CONFIG_DIR) home --target=$(HOME_DIR)
 else ifeq ($(TARGET),etc)
+	# Restow dotfiles-nix/etc
 	sudo stow -v --delete --dir=$(CONFIG_DIR) etc --target=/etc
 else ifeq ($(TARGET),usr)
+	# Restow dotfiles-nix/usr
 	sudo stow -v --delete --dir=$(CONFIG_DIR) usr --target=/usr
 else ifeq ($(TARGET),personal)
-	stow -v --delete --dir=$(PERSONAL_DIR) . --target=$(HOME_DIR)
+	# Restow Personal/home
+	stow -v --delete --dir=$(PERSONAL_DIR) home --target=$(HOME_DIR)
+else ifeq ($(TARGET),personal-adopt)
+	# Restow Personal/home-adopt (for pesky apps who break symlinks on their configs)
+	stow -v --delete --dir=$(PERSONAL_DIR) home-adopt --target=$(HOME_DIR)
+else ifeq ($(TARGET),adopt)
+	# Restow Personal/home-adopt (for pesky apps who break symlinks on their configs)
+	stow -v --delete --dir=$(PERSONAL_DIR) home-adopt --target=$(HOME_DIR)
 else
 	@echo -e "\033[1;31mInvalid target '${TARGET}'\033[0m"
 	@echo -e "\033[1;31mValid options: all, home, etc, usr\033[0m"
