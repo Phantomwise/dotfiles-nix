c0738f3377b23ac5b176dabe8e9844f3675b0181
Phantomwise <phantomwise67@protonmail.com>
2025-07-14 17:34:10 +0200

    🔧 Add personal `/etc` to restow

--- nix/stow.mk
+++ nix/stow.mk
@@ -13,6 +13,7 @@ ifeq ($(TARGET),all)
 	sudo stow -v --restow --dir=$(CONFIG_DIR) usr --target=/usr
 	stow -v --restow --dir=$(PERSONAL_DIR) home --target=$(HOME_DIR)
 	stow -v --restow --adopt --dir=$(PERSONAL_DIR) home-adopt --target=$(HOME_DIR)
+	sudo stow -v --restow --dir=$(PERSONAL_DIR) etc --target=/etc
 else ifeq ($(TARGET),home)
 	# Restow dotfiles-nix/home
 	stow -v --restow --dir=$(CONFIG_DIR) home --target=$(HOME_DIR)
@@ -28,6 +29,9 @@ else ifeq ($(TARGET),personal)
 else ifeq ($(TARGET),personal-adopt)
 	# Restow Personal/home-adopt (for pesky apps who break symlinks on their configs)
 	stow -v --restow --adopt --dir=$(PERSONAL_DIR) home-adopt --target=$(HOME_DIR)
+else ifeq ($(TARGET),personal-etc)
+	# Restow Personal/etc
+	sudo stow -v --restow --dir=$(PERSONAL_DIR) etc --target=/etc
 else ifeq ($(TARGET),adopt)
 	# Restow Personal/home-adopt (for pesky apps who break symlinks on their configs)
 	stow -v --restow --adopt --dir=$(PERSONAL_DIR) home-adopt --target=$(HOME_DIR)
@@ -45,7 +49,9 @@ ifeq ($(TARGET),all)
 	sudo stow -v --delete --dir=$(CONFIG_DIR) etc --target=/etc
 	sudo stow -v --delete --dir=$(CONFIG_DIR) usr --target=/usr
 	stow -v --delete --dir=$(PERSONAL_DIR) home --target=$(HOME_DIR)
+	stow -v --delete --dir=$(PERSONAL_DIR) etc --target=$/etc
 	stow -v --delete --dir=$(PERSONAL_DIR) home-adopt --target=$(HOME_DIR)
+	sudo stow -v --delete --dir=$(PERSONAL_DIR) etc --target=/etc
 else ifeq ($(TARGET),home)
 	# Restow dotfiles-nix/home
 	stow -v --delete --dir=$(CONFIG_DIR) home --target=$(HOME_DIR)
@@ -61,6 +67,9 @@ else ifeq ($(TARGET),personal)
 else ifeq ($(TARGET),personal-adopt)
 	# Restow Personal/home-adopt (for pesky apps who break symlinks on their configs)
 	stow -v --delete --dir=$(PERSONAL_DIR) home-adopt --target=$(HOME_DIR)
+else ifeq ($(TARGET),personal-etc)
+	# Restow Personal/etc
+	sudo stow -v --delete --dir=$(PERSONAL_DIR) etc --target=/etc
 else ifeq ($(TARGET),adopt)
 	# Restow Personal/home-adopt (for pesky apps who break symlinks on their configs)
 	stow -v --delete --dir=$(PERSONAL_DIR) home-adopt --target=$(HOME_DIR)
