2d3d6113ba1e6a84e33a797565752c5eccde46aa
Phantomwise <phantomwise67@protonmail.com>
2025-07-14 20:13:35 +0200

    ♻️ Split user config from configuration.nix to `users.nix`

---
+++ nix/etc/nixos/users.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+	# Define a user account. Don't forget to set a password with ‘passwd’.
+	users.users.phantomwise = {
+		isNormalUser = true;
+		description = "Phantomwise";
+		extraGroups = [ "networkmanager" "wheel" ];
+		packages = with pkgs; [];
+		shell = pkgs.zsh;
+	};
+
+	users.defaultUserShell = pkgs.zsh;
+
+}
