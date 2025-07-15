a7062b38bd0174c9923ecf79062dbc2ebe19fd20
Phantomwise <phantomwise67@protonmail.com>
2025-07-15 23:29:23 +0200

    ♻️ Move firewall config to `networking.nix`

--- nix/etc/nixos/networking.nix
+++ nix/etc/nixos/networking.nix
@@ -10,4 +10,10 @@
 	# Enable systemd resolved
 	services.resolved.enable = true;
 
+	# Open ports in the firewall.
+	# networking.firewall.allowedTCPPorts = [ ... ];
+	# networking.firewall.allowedUDPPorts = [ ... ];
+	# Or disable the firewall altogether.
+	# networking.firewall.enable = false;
+
 }
