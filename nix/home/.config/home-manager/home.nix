8e7c5cc1d471988206adc4670492215ae33e208f
Phantomwise <phantomwise67@protonmail.com>
2025-07-20 22:28:01 +0200

    🎉 Make home manager manage Sway

--- nix/home/.config/home-manager/home.nix
+++ nix/home/.config/home-manager/home.nix
@@ -5,6 +5,7 @@
 	imports =
 	[
 		./zsh.nix
+		./sway/sway-config.nix
 	];
 
 	# Home Manager needs a bit of information about you and the paths it should
@@ -85,4 +86,5 @@
 
 	# Let Home Manager install and manage itself.
 	programs.home-manager.enable = true;
+	programs.home-manager.path = "$HOME/src/github.com/nix-community/home-manager";
 }
