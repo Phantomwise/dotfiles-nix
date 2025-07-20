048441134a93f085123451c6a66d263dc3d7dad5
Phantomwise <phantomwise67@protonmail.com>
2025-07-20 23:57:37 +0200

    🚧 Convert Sway keybindings to Home Manager (WIP)

--- nix/home/.config/home-manager/sway/sway-keybindings.nix
+++ nix/home/.config/home-manager/sway/sway-keybindings.nix
@@ -1,3 +1,21 @@
+# ███████████████████████████████████████████████████████████████╗
+# █╔════════════════════════════════════════════════════════════█║
+# █║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█║
+# █║░░░░░░░░░░░░░███████╗██╗░░░░██╗░█████╗░██╗░░░██╗░░░░░░░░░░░░█║
+# █║░░░░░░░░░░░░░██╔════╝██║░░░░██║██╔══██╗╚██╗░██╔╝░░░░░░░░░░░░█║
+# █║░░░░░░░░░░░░░███████╗██║░█╗░██║███████║░╚████╔╝░░░░░░░░░░░░░█║
+# █║░░░░░░░░░░░░░╚════██║██║███╗██║██╔══██║░░╚██╔╝░░░░░░░░░░░░░░█║
+# █║░░░░░░░░░░░░░███████║╚███╔███╔╝██║░░██║░░░██║░░░░░░░░░░░░░░░█║
+# █║░░░░░░░░░░░░░╚══════╝░╚══╝╚══╝░╚═╝░░╚═╝░░░╚═╝░░░░░░░░░░░░░░░█║
+# █║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█║
+# █║░░░░░░░░░░░░░░░░░░░░░░░ Home Manager ░░░░░░░░░░░░░░░░░░░░░░░█║
+# █║░░░░░░░░░░░░░░░░░░░░ Sway configuration ░░░░░░░░░░░░░░░░░░░░█║
+# █║░░░░░░░░░░░░░░░░░░░░░░░ Keybindings ░░░░░░░░░░░░░░░░░░░░░░░░█║
+# █║░░░░░░░░░░░░░░░░░░░░░░ by Phantomwise ░░░░░░░░░░░░░░░░░░░░░░█║
+# █║░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█║
+# ███████████████████████████████████████████████████████████████║
+# ╚══════════════════════════════════════════════════════════════╝
+
 { config, pkgs, lib, ... }:
 
 let
@@ -74,6 +92,14 @@ in
 		# Kill focused window
 		"${mod}+Shift+q" = "kill";
 
+	# Mouse bindings
+		# Mouse bindings operate on the container under the cursor instead of the container that has fo‐
+		# cus. Mouse buttons can either be specified in the form button[1-9] or by using the name of the
+		# event code (ex BTN_LEFT or BTN_RIGHT). For the former option, the buttons will  be  mapped  to
+		# their  values  in  X11  (1=left, 2=middle, 3=right, 4=scroll up, 5=scroll down, 6=scroll left,
+		# 7=scroll right, 8=back, 9=forward). For the latter option, you can find the event names  using
+		# libinput debug-events.
+
 	# ███████████████████████████████████████████████████████████████╗
 	# █╔════════════════════════════════════════════════════════════█║
 	# █║░░░░░░░░░░░░░░░░░░░░░░░ APPLICATIONS ░░░░░░░░░░░░░░░░░░░░░░░█║
@@ -94,7 +120,7 @@ in
 		"Ctrl+Alt+v" = "exec ${videoplayer}";
 
 # Start your sway-bar
-# bindsym $mod+d exec $menu
+# "${mod}+d" = "exec $menu";
 
 	# Start rofi
 		"${mod}+r" = "exec ${runmenu}";
@@ -142,5 +168,152 @@ in
 		"${mod}+Shift+Up" = "move up";
 		"${mod}+Shift+Right" = "move right";
 
+	# ███████████████████████████████████████████████████████████████╗
+	# █╔════════════════════════════════════════════════════════════█║
+	# █║░░░░░░░░░░░░░░░░░░░░░░░░ WORKSPACES ░░░░░░░░░░░░░░░░░░░░░░░░█║
+	# ███████████████████████████████████████████████████████████████║
+	# ╚══════════════════════════════════════════════════════════════╝
+
+	# Switch to workspace
+		"${mod}+1" = "workspace number 1";
+		"${mod}+2" = "workspace number 2";
+		"${mod}+3" = "workspace number 3";
+		"${mod}+4" = "workspace number 4";
+		"${mod}+5" = "workspace number 5";
+		"${mod}+6" = "workspace number 6";
+		"${mod}+7" = "workspace number 7";
+		"${mod}+8" = "workspace number 8";
+		"${mod}+9" = "workspace number 9";
+		"${mod}+0" = "workspace number 10";
+
+	# Cycle through workspaces
+		"${mod}+Tab" = "workspace next";
+		"${mod}+Prior" = "workspace prev";
+		"${mod}+Next" = "workspace next";
+
+	# Cycle through workspaces with the mouse
+		# "${mod}+button4" = "workspace prev";
+		# "${mod}+button5" = "workspace next";
+	# NOTWORKING
+	# Original config:
+		# Cycle through workspaces with the mouse
+		# bindsym --whole-window {
+			# $mod+button4 workspace prev
+			# $mod+button5 workspace next
+		# }
+
+	# Move focused container to workspace
+		"${mod}+Shift+1" = "move container to workspace number 1";
+		"${mod}+Shift+2" = "move container to workspace number 2";
+		"${mod}+Shift+3" = "move container to workspace number 3";
+		"${mod}+Shift+4" = "move container to workspace number 4";
+		"${mod}+Shift+5" = "move container to workspace number 5";
+		"${mod}+Shift+6" = "move container to workspace number 6";
+		"${mod}+Shift+7" = "move container to workspace number 7";
+		"${mod}+Shift+8" = "move container to workspace number 8";
+		"${mod}+Shift+9" = "move container to workspace number 9";
+		"${mod}+Shift+0" = "move container to workspace number 10";
+	# Note: workspaces can have any name you want, not just numbers.
+	# We just use 1-10 as the default.
+
+	# ███████████████████████████████████████████████████████████████╗
+	# █╔════════════════════════════════════════════════════════════█║
+	# █║░░░░░░░░░░░░░░░░░░░░░░░░░░ LAYOUT ░░░░░░░░░░░░░░░░░░░░░░░░░░█║
+	# ███████████████████████████████████████████████████████████████║
+	# ╚══════════════════════════════════════════════════════════════╝
+
+	# You can "split" the current object of your focus with
+	# $mod+b or $mod+v, for horizontal and vertical splits
+	# respectively.
+		"${mod}+h" = "splith";
+		"${mod}+v" = "splitv";
+
+	# layout default|splith|splitv|stacking|tabbed
+		# Sets the layout mode of the focused container.
+
+	# Switch the current container between different layout styles
+	# "${mod}+d layout default
+		"${mod}+alt+s" = "layout stacking";
+		"${mod}+alt+t" = "layout tabbed";
+		"${mod}+alt+e" = "layout toggle split";
+		"${mod}+alt+h" = "layout splith";
+		"${mod}+alt+v" = "layout splitv";
+
+	# layout toggle [split|all]
+		# Cycles  the  layout mode of the focused container though a preset list of layouts. If no argu‐
+		# ment is given, then it cycles through stacking, tabbed and the last split layout. If split  is
+		# given, then it cycles through splith and splitv. If all is given, then it cycles through every
+		# layout.
+		"${mod}+l" = "layout toggle all";
+		# "${mod}+p" = "layout toggle split";
+
+	# Cycles the layout mode of the focused container through a list of layouts.
+		# layout toggle [split|tabbed|stacking|splitv|splith] [split|tabbed|stacking|splitv|splith]...
+
+	# Make the current focus fullscreen
+	"${mod}+f" = "fullscreen";
+
+	# Toggle the current focus between tiling and floating mode
+	"${mod}+Shift+space" = "floating toggle";
+
+	# Swap focus between the tiling area and the floating area
+	"${mod}+space" = "focus mode_toggle";
+
+	# Move focus to the parent container
+	"${mod}+a" = "focus parent";
+
+	# ███████████████████████████████████████████████████████████████╗
+	# █╔════════════════════════════════════════════════════════════█║
+	# █║░░░░░░░░░░░░░░░░░░░░░░░░ SCRATCHPAD ░░░░░░░░░░░░░░░░░░░░░░░░█║
+	# ███████████████████████████████████████████████████████████████║
+	# ╚══════════════════════════════════════════════════════════════╝
+
+	# Sway has a "scratchpad", which is a bag of holding for windows.
+	# You can send windows there and get them back later.
+
+	# Move the currently focused window to the scratchpad
+	"${mod}+Shift+minus" = "move scratchpad";
+
+	# Show the next scratchpad window or hide the focused scratchpad window.
+	# If there are multiple scratchpad windows, this command cycles through them.
+	"${mod}+minus" = "scratchpad show";
+
+	# ███████████████████████████████████████████████████████████████╗
+	# █╔════════════════════════════════════════════════════════════█║
+	# █║░░░░░░░░░░░░░░░░░░░░░░░░░░ RESIZE ░░░░░░░░░░░░░░░░░░░░░░░░░░█║
+	# ███████████████████████████████████████████████████████████████║
+	# ╚══════════════════════════════════════════════════════════════╝
+
+	# ███████████████████████████████████████████████████████████████╗
+	# █╔════════════════════════════════════════════════════════════█║
+	# █║░░░░░░░░░░░░░░░░░░░░░░░░░░ VOLUME ░░░░░░░░░░░░░░░░░░░░░░░░░░█║
+	# ███████████████████████████████████████████████████████████████║
+	# ╚══════════════════════════════════════════════════════════════╝
+
+	# ███████████████████████████████████████████████████████████████╗
+	# █╔════════════════════════════════════════════════════════════█║
+	# █║░░░░░░░░░░░░░░░░░░░░ SCREEN BRIGHTNESS ░░░░░░░░░░░░░░░░░░░░░█║
+	# ███████████████████████████████████████████████████████████████║
+	# ╚══════════════════════════════════════════════════════════════╝
+
+	# ███████████████████████████████████████████████████████████████╗
+	# █╔════════════════════════════════════════════════════════════█║
+	# █║░░░░░░░░░░░░░░░░░░░░░░░ SCREENSHOTS ░░░░░░░░░░░░░░░░░░░░░░░░█║
+	# ███████████████████████████████████████████████████████████████║
+	# ╚══════════════════════════════════════════════════════════════╝
+
+	# ███████████████████████████████████████████████████████████████╗
+	# █╔════════════════════════════════════════════════════════════█║
+	# █║░░░░░░░░░░░░░░░░░░░░░░░░ WALLPAPER ░░░░░░░░░░░░░░░░░░░░░░░░░█║
+	# █║░░░░░░░░░░░░░░░░░░░░░░░░░░ SWAYBG ░░░░░░░░░░░░░░░░░░░░░░░░░░█║
+	# ███████████████████████████████████████████████████████████████║
+	# ╚══════════════════════════════════════════════════════════════╝
+
 	};
 }
+
+# ███████████████████████████████████████████████████████████████╗
+# █╔════════════════════════════════════════════════════════════█║
+# █║░░░░░░░░░░░░░░░░░░░░░░░░░░░ END ░░░░░░░░░░░░░░░░░░░░░░░░░░░░█║
+# ███████████████████████████████████████████████████████████████║
+# ╚══════════════════════════════════════════════════════════════╝
