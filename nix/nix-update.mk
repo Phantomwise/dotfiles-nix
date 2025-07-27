12c3e91f1c737cead61b648071c235bb4561a4ef
Phantomwise <phantomwise67@protonmail.com>
2025-07-27 06:35:01 +0200

    🔧 Add task to update system channels

--- nix/nix-update.mk
+++ nix/nix-update.mk
@@ -5,5 +5,6 @@
 nix-update:
 	@echo -e "\033[1;33mUpdating channels\033[0m"
 	nix-channel --update
+	sudo nix-channel --update
 	@echo -e "\033[1;33mRebuilding\033[0m"
 	sudo nixos-rebuild switch --upgrade
