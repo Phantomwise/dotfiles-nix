4d5a3c093e46de303664d345bc23a6704c62a3f1
Phantomwise <phantomwise67@protonmail.com>
2025-07-27 06:31:49 +0200

    ⚗️ Add a file for unstable packages

---
+++ nix/etc/nixos/packages-unstable.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+let
+	unstable = import <nixos-unstable> {
+		config = config.nixpkgs.config;  # Reuse the same config already defined
+		system = pkgs.system;            # Reuse the same architecture already defined
+	};
+in {
+
+	environment.systemPackages = with unstable; [
+		# unstable packages
+	];
+}
