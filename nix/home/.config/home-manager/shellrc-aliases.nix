7e2f2336a1999a7cdb05f4606d507c91349d83d9
Phantomwise <phantomwise67@protonmail.com>
2025-07-28 13:31:08 +0200

    💡 Sort aliases by category

--- nix/home/.config/home-manager/shellrc-aliases.nix
+++ nix/home/.config/home-manager/shellrc-aliases.nix
@@ -1,13 +1,20 @@
 {
+	# Directories
 	"~" = "cd ~";
 	".." = "cd ..";
 	cdd = "cd ~/Sync/SynologyDrive/dotfiles-nix/nix/";
 	cds = "cd ~/Sync/SynologyDrive/";
 	cdy = "cd ~/Downloads/yt-dlp/";
+
+	# Commands replacement
 	ll = "ls -alh";
 	grep = "grep --color=auto";
+
+	# Nix commands
 	hh = "home-manager switch";
 	rebuild = "sudo nixos-rebuild switch";
 	update = "sudo nixos-rebuild switch --upgrade";
-	"echo $PATH" = "echo $PATH | tr ':' '\n'";
+
+	# Other
+	"echo $PATH" = "echo $PATH | tr ':' '\n'"; # NOTWORKING
 }
