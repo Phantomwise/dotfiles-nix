d2b54d492453bb21bcb5dd9905c1c426c082e1b2
Phantomwise <phantomwise67@protonmail.com>
2025-07-28 13:28:18 +0200

    🔧 Add shell aliases

--- nix/home/.config/home-manager/shellrc-aliases.nix
+++ nix/home/.config/home-manager/shellrc-aliases.nix
@@ -3,7 +3,11 @@
 	".." = "cd ..";
 	cdd = "cd ~/Sync/SynologyDrive/dotfiles-nix/nix/";
 	cds = "cd ~/Sync/SynologyDrive/";
+	cdy = "cd ~/Downloads/yt-dlp/";
 	ll = "ls -alh";
+	grep = "grep --color=auto";
 	hh = "home-manager switch";
-	update = "sudo nixos-rebuild switch";
+	rebuild = "sudo nixos-rebuild switch";
+	update = "sudo nixos-rebuild switch --upgrade";
+	"echo $PATH" = "echo $PATH | tr ':' '\n'";
 }
