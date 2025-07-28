a271cddee7a4f869ff6498b6bf630ab9634a87cd
Phantomwise <phantomwise67@protonmail.com>
2025-07-28 13:20:45 +0200

    ♻️ Move shell aliases to a separate file

---
+++ nix/home/.config/home-manager/shellrc-aliases.nix
@@ -0,0 +1,9 @@
+{
+	"~" = "cd ~";
+	".." = "cd ..";
+	cdd = "cd ~/Sync/SynologyDrive/dotfiles-nix/nix/";
+	cds = "cd ~/Sync/SynologyDrive/";
+	ll = "ls -alh";
+	hh = "home-manager switch";
+	update = "sudo nixos-rebuild switch";
+}
