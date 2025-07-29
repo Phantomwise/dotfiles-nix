23f8f506627a984e8e9ef80f770a1908dd363729
Phantomwise <phantomwise67@protonmail.com>
2025-07-29 21:46:37 +0200

    🔧 Add aliases for clamscan

--- nix/home/.config/home-manager/shellrc-aliases.nix
+++ nix/home/.config/home-manager/shellrc-aliases.nix
@@ -9,6 +9,8 @@
 	# Commands replacement
 	ll = "ls -alh";
 	grep = "grep --color=auto";
+	"clamscan" = "clamscan -r --log=/tmp/clamscan.txt";
+	"clamscan-full" = "clamscan -r --follow-dir-symlinks --follow-file-symlinks --log=/tmp/clamscan.txt";
 
 	# Nix commands
 	hh = "home-manager switch";
