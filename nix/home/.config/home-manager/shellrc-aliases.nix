ea023ccae92bcb88f1517ce93c03904313a09645
Phantomwise <phantomwise67@protonmail.com>
2025-08-03 07:59:32 +0200

    ✨ Add alias for curl weather check

--- nix/home/.config/home-manager/shellrc-aliases.nix
+++ nix/home/.config/home-manager/shellrc-aliases.nix
@@ -11,6 +11,7 @@
 	grep = "grep --color=auto";
 	"clamscan" = "clamscan -r --log=/tmp/clamscan.txt";
 	"clamscan-full" = "clamscan -r --follow-dir-symlinks --follow-file-symlinks --log=/tmp/clamscan.txt";
+	weather = "curl wttr.in";
 
 	# Nix commands
 	hh = "home-manager switch";
