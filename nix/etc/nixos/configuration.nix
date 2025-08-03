a8965ea07710491fb41eb463f775b2d956d97cf4
Phantomwise <phantomwise67@protonmail.com>
2025-08-03 07:08:37 +0200

    💡 Add comments

--- nix/etc/nixos/configuration.nix
+++ nix/etc/nixos/configuration.nix
@@ -46,6 +46,7 @@
 	# networking.proxy.default = "http://user:password@proxy:port/";
 	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+	# Auto Update
 	system.autoUpgrade.enable = true;
 	system.autoUpgrade.dates = "weekly";
 
