917455e88d39f0076042844eb4af145b55fd6ca8
Phantomwise <phantomwise67@protonmail.com>
2025-08-03 07:05:24 +0200

    ✨ Add task to uninstall unused flatpak dependencies

--- nix/nix-cleanup.mk
+++ nix/nix-cleanup.mk
@@ -4,6 +4,8 @@
 # nix-cleanup
 nix-cleanup:
 
+	@echo -e "\033[1;33mUninstall unused flatpak dependencies\033[0m"
+	flatpak uninstall --unused
 	@echo -e "\033[1;33mDeleting old system generations (keep last 10)\033[0m"
 	sudo nix-env --delete-generations +10 --profile /nix/var/nix/profiles/system
 	@echo -e "\033[1;33mDeleting old user generations (keep last 10)\033[0m"
@@ -12,6 +14,8 @@ nix-cleanup:
 	sudo nix-collect-garbage --delete-older-than 7d
 	@echo -e "\033[1;33mCleaning up unused user paths\033[0m"
 	nix-collect-garbage --delete-older-than 7d
+	@echo -e "\033[1;33mDeleting old home manager generations (keep last 7 days)\033[0m"
+	home-manager expire-generations "-7 days"
 	@echo -e "\033[1;33mDeduplication\033[0m"
 	sudo nix-store --optimise
 	@echo -e "\033[1;33mListing remaining user generations\033[0m"
