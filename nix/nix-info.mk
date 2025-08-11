ca82f0c23805bebc351204baca0b25128199d351
Phantomwise <phantomwise67@protonmail.com>
2025-08-11 06:27:07 +0200

    ✨ Add tasks to list generations

--- nix/nix-info.mk
+++ nix/nix-info.mk
@@ -7,3 +7,7 @@ nix-info:
 	sudo nix-channel --list
 	@echo -e "\033[1;33mList user channels\033[0m"
 	nix-channel --list
+	@echo -e "\033[1;33mList system generations\033[0m"
+	sudo nix-env --list-generations
+	@echo -e "\033[1;33mList user generations\033[0m"
+	nix-env --list-generations
