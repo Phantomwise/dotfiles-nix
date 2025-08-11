6c753835abccaaf9b6b7bb4f9220b5ca0ce4f185
Phantomwise <phantomwise67@protonmail.com>
2025-08-11 06:26:42 +0200

    💄 Add echo messages for each task

--- nix/nix-info.mk
+++ nix/nix-info.mk
@@ -3,6 +3,7 @@
 
 # nix-info
 nix-info:
-
-	nix-channel --list
+	@echo -e "\033[1;33mList system channels\033[0m"
 	sudo nix-channel --list
+	@echo -e "\033[1;33mList user channels\033[0m"
+	nix-channel --list
