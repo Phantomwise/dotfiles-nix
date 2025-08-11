b068c48b8266f6b5ec68df7fcbcc9e0efb403c86
Phantomwise <phantomwise67@protonmail.com>
2025-08-11 07:58:51 +0200

    ✨ Add rclone task for Firefox profile

--- nix/rclone.mk
+++ nix/rclone.mk
@@ -6,5 +6,7 @@ SYNC_SRC_DIR := /home/phantomwise/Sync/
 
 # Sync with rclone
 rclone:
-	@echo -e "\033[1;33mSyncing\033[0m"
+	@echo -e "\033[1;33mSyncing Firefox\033[0m"
+	rclone sync -v /home/phantomwise/.mozilla/firefox/h0gi4v0t.Phantomwise/ $(SYNC_SRC_DIR)/Additional/Firefox
+	@echo -e "\033[1;33mSyncing to NAS\033[0m"
 	rclone sync -v $(SYNC_SRC_DIR) NAS:personal/Sync/Dell-Latitude-E5530-Nix-Rclone
