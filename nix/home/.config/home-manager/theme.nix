babdbd9835fc5c6596ef5ab5f1d78d87e59df3a0
Phantomwise <phantomwise67@protonmail.com>
2025-08-19 10:52:59 +0200

    🎨 Restructure themes list

--- nix/home/.config/home-manager/theme.nix
+++ nix/home/.config/home-manager/theme.nix
@@ -8,27 +8,29 @@
 
 		theme = {
 			# Flat Remix
-			# https://github.com/daniruiz/flat-remix-gtk/tree/master/themes
-			name = "Flat-Remix-GTK-White-Darkest-Solid";
-			package = pkgs.flat-remix-gtk;
-			# https://github.com/EliverLara/Sweet
-			# name = "Sweet-Dark-v40";
-			# package = pkgs.sweet;
+				# https://github.com/daniruiz/flat-remix-gtk/tree/master/themes
+				package = pkgs.flat-remix-gtk;
+				name = "Flat-Remix-GTK-White-Darkest-Solid";
+			# Sweet
+				# https://github.com/EliverLara/Sweet
+				# package = pkgs.sweet;
+				# name = "Sweet-Dark-v40";
 			};
 
 		iconTheme = {
 			# Material-Black
-			# https://github.com/rtlewis88/rtl88-Themes/tree/material-black-COLORS
-			# name = "Material-Black-Mango-Numix";
-			# name = "Material-Black-Mango-Suru";
-			# name = "MB-Mango-Suru-GLOW";
-			# package = pkgs.material-black-colors;
-			# https://github.com/EliverLara/Sweet-folders
-			name = "Sweet-Rainbow";
-			# name = "Sweet-Blue-Filled";
-			# name = "Sweet-Yellow";
-			# name = "Sweet-Yellow-Filled";
-			package = pkgs.sweet-folders;
+				# https://github.com/rtlewis88/rtl88-Themes/tree/material-black-COLORS
+				# package = pkgs.material-black-colors;
+				# name = "Material-Black-Mango-Numix";
+				# name = "Material-Black-Mango-Suru";
+				# name = "MB-Mango-Suru-GLOW";
+			# Sweet
+				# https://github.com/EliverLara/Sweet-folders
+				package = pkgs.sweet-folders;
+				name = "Sweet-Rainbow";
+				# name = "Sweet-Blue-Filled";
+				# name = "Sweet-Yellow";
+				# name = "Sweet-Yellow-Filled";
 			};
 
 	};
