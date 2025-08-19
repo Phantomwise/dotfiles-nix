e1dda8cbfc2532858599cb1a25ee6e52a5e5a7f4
Phantomwise <phantomwise67@protonmail.com>
2025-08-19 10:51:56 +0200

    💄 Add GTK themes

--- nix/home/.config/home-manager/theme.nix
+++ nix/home/.config/home-manager/theme.nix
@@ -7,12 +7,22 @@
 		enable = true;
 
 		theme = {
+			# Flat Remix
+			# https://github.com/daniruiz/flat-remix-gtk/tree/master/themes
+			name = "Flat-Remix-GTK-White-Darkest-Solid";
+			package = pkgs.flat-remix-gtk;
 			# https://github.com/EliverLara/Sweet
-			name = "Sweet-Dark-v40";
-			package = pkgs.sweet;
+			# name = "Sweet-Dark-v40";
+			# package = pkgs.sweet;
 			};
 
 		iconTheme = {
+			# Material-Black
+			# https://github.com/rtlewis88/rtl88-Themes/tree/material-black-COLORS
+			# name = "Material-Black-Mango-Numix";
+			# name = "Material-Black-Mango-Suru";
+			# name = "MB-Mango-Suru-GLOW";
+			# package = pkgs.material-black-colors;
 			# https://github.com/EliverLara/Sweet-folders
 			name = "Sweet-Rainbow";
 			# name = "Sweet-Blue-Filled";
