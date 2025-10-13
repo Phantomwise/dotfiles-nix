61d91d18b3f0baa30d8ad636b7608c426cd7c4bd
Phantomwise <phantomwise67@protonmail.com>
2025-10-13 02:09:42 +0200

    🎨 Replace spaces with tabs

--- nix/home/.local/bin/scripts/youtube-download-video.sh
+++ nix/home/.local/bin/scripts/youtube-download-video.sh
@@ -17,41 +17,41 @@ read -e -p "Enter YouTube URL or ID: " input
 
 # Function to check if the URL is a playlist
 function is_playlist {
-    if [[ $1 == *"list="* ]]; then
-        return 0
-    else
-        return 1
-    fi
+	if [[ $1 == *"list="* ]]; then
+		return 0
+	else
+		return 1
+	fi
 }
 
 # Check if the input is a URL or an ID
 if [[ $input == *"youtube.com"* ]] || [[ $input == *"youtu.be"* ]]; then
-    url="$input"
+	url="$input"
 else
-    url="https://www.youtube.com/watch?v=$input"
+	url="https://www.youtube.com/watch?v=$input"
 fi
 
 # If the URL is a playlist, prompt the user to choose whether to download only the video or the full playlist
 if is_playlist "$url"; then
-    echo -e "${info} This URL links to a playlist."
-    read -p "Do you want to download 1. only the video or 2. the full playlist? (Enter 1 or 2, default is 1): " choice
-    choice=${choice:-1}
-    if [[ $choice == "1" ]]; then
-        echo -e "${info} Downloading only the video."
-        url=$(echo "$url" | sed 's/\&list=.*/\&/g')
-        # Remove trailing & or ? if they are left at the end
-        url=$(echo "$url" | sed 's/[?&]$//')
-    else
-        echo -e "${info} Downloading the full playlist."
-    fi
+	echo -e "${info} This URL links to a playlist."
+	read -p "Do you want to download 1. only the video or 2. the full playlist? (Enter 1 or 2, default is 1): " choice
+	choice=${choice:-1}
+	if [[ $choice == "1" ]]; then
+		echo -e "${info} Downloading only the video."
+		url=$(echo "$url" | sed 's/\&list=.*/\&/g')
+		# Remove trailing & or ? if they are left at the end
+		url=$(echo "$url" | sed 's/[?&]$//')
+	else
+		echo -e "${info} Downloading the full playlist."
+	fi
 fi
 
 # Define function to download the best video format with subtitles and impersonate Chrome + Windows 10
 function download_video_w_sub {
-    echo -e "${info} Running yt-dlp to download video with subtitles:"
-    yt-dlp -f - -o "%(title)s [YouTube] [] [%(id)s] [%(format_id)s].%(ext)s" --write-subs --sub-langs "all" --impersonate="chrome:windows-10" --sleep-interval 20 --max-sleep-interval 40 --limit-rate 600K "$url" && \
-    echo -e "${succ} Download video with subtitles successful." || \
-    { echo -e "${err} Error while downloading video with subtitles."; return 1; }
+	echo -e "${info} Running yt-dlp to download video with subtitles:"
+	yt-dlp -f - -o "%(title)s [YouTube] [] [%(id)s] [%(format_id)s].%(ext)s" --write-subs --sub-langs "all" --impersonate="chrome:windows-10" --sleep-interval 20 --max-sleep-interval 40 --limit-rate 600K "$url" && \
+	echo -e "${succ} Download video with subtitles successful." || \
+	{ echo -e "${err} Error while downloading video with subtitles."; return 1; }
 }
 
 # Run each command in order, continue on failure
