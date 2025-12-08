57d98d040edc8c2b6bef3d115e69da22b1284e84
Phantomwise <phantomwise67@protonmail.com>
2025-12-08 12:09:54 +0100

    ✨ Add yt-dlp wrappers

---
+++ nix/home/.local/bin/scripts/yt-dlp-wrapper-audio.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+def main():
+    url = input("Enter the video URL: ").strip()
+    # output_template = "%(title)s [%(id)s] [%(extractor)s, %(channel_id)s] [%(format_id)s].%(ext)s"
+    output_template = "%(title)s [%(id)s] [%(extractor)s, %(uploader,uploader_id,channel,channel_id)s] [%(format_id)s].%(ext)s"
+    # output_template = "%(title)s [%(extractor)s, %(uploader,uploader_id,channel,channel_id)s] [%(format_id)s].%(ext)s"
+    # output_template = "%(title)s [%(webpage_url_domain)s, %(channel_id)s] [%(format_id)s].%(ext)s"
+    # output_template = "%(title)s [%(webpage_url_domain)s, %(uploader,uploader_id,channel,channel_id)s] [%(format_id)s].%(ext)s"
+    cmd = [
+        "yt-dlp",
+        "-x",
+        "-o", output_template,
+        url
+    ]
+    subprocess.run(cmd)
+
+if __name__ == "__main__":
+    main()
