5a37399f26cfde6bb0bb0fb265a982f1de3a8f0d
Phantomwise <phantomwise67@protonmail.com>
2025-12-09 01:41:56 +0100

    🔧 Update yt-dlp wrappers

--- nix/home/.local/bin/scripts/yt-dlp-wrapper-audio.py
+++ nix/home/.local/bin/scripts/yt-dlp-wrapper-audio.py
@@ -5,7 +5,7 @@ import subprocess
 def main():
     url = input("Enter the video URL: ").strip()
     # output_template = "%(title)s [%(id)s] [%(extractor)s, %(channel_id)s] [%(format_id)s].%(ext)s"
-    output_template = "%(title)s [%(id)s] [%(extractor)s, %(uploader,uploader_id,channel,channel_id)s] [%(format_id)s].%(ext)s"
+    output_template = "%(title)s [%(extractor)s, %(uploader_id,channel_id,uploader,channel)s] [%(id)s] [%(format_id)s].%(ext)s"
     # output_template = "%(title)s [%(extractor)s, %(uploader,uploader_id,channel,channel_id)s] [%(format_id)s].%(ext)s"
     # output_template = "%(title)s [%(webpage_url_domain)s, %(channel_id)s] [%(format_id)s].%(ext)s"
     # output_template = "%(title)s [%(webpage_url_domain)s, %(uploader,uploader_id,channel,channel_id)s] [%(format_id)s].%(ext)s"
