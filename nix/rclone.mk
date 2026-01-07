c3556b1e61a1c0741b3e867267c67b639eb58845
Phantomwise <phantomwise67@protonmail.com>
2026-01-07 16:25:12 +0100

    🐛 Fix permission problem

--- nix/rclone.mk
+++ nix/rclone.mk
@@ -6,9 +6,24 @@ SYNC_SRC_DIR := /home/phantomwise/Sync/
 
 # Sync with rclone
 rclone:
-	@echo -e "\033[1;33mSyncing Firefox\033[0m"
-	rclone sync -v /home/phantomwise/.mozilla/firefox/h0gi4v0t.Phantomwise/ $(SYNC_SRC_DIR)/Additional/Firefox
+	@echo -e "\033[1;33mTemporarily changing ownership of the Secrets folder\033[0m"
+	sudo chown -R phantomwise $(SYNC_SRC_DIR)Personal/home/Secrets
 	@echo -e "\033[1;33mSyncing to NAS\033[0m"
 	rclone sync -v $(SYNC_SRC_DIR) NAS:personal/Sync/Dell-Latitude-E5530-Nix-Rclone
+	@echo -e "\033[1;33mReverting ownership\033[0m"
+	sudo chown -R root:root $(SYNC_SRC_DIR)Personal/home/Secrets
+	@echo -e "\033[1;33mSyncing to NAS (Projects)\033[0m"
+	rclone sync -v $(SYNC_SRC_DIR)Personal/home/Projects NAS:personal/Projects
+
+# Sync with rclone
+rclone-all:
+	@echo -e "\033[1;33mSyncing Firefox to Sync folder\033[0m"
+	rclone sync -v /home/phantomwise/.mozilla/firefox/h0gi4v0t.Phantomwise/ $(SYNC_SRC_DIR)/Additional/Firefox
+	@echo -e "\033[1;33mTemporarily changing ownership of the Secrets folder\033[0m"
+	sudo chown -R phantomwise $(SYNC_SRC_DIR)Personal/home/Secrets
 	@echo -e "\033[1;33mSyncing to NAS\033[0m"
-	rclone sync -v $(SYNC_SRC_DIR)/Personal/home/Projects NAS:personal/Projects
+	rclone sync -v $(SYNC_SRC_DIR) NAS:personal/Sync/Dell-Latitude-E5530-Nix-Rclone
+	@echo -e "\033[1;33mReverting ownership\033[0m"
+	sudo chown -R root:root $(SYNC_SRC_DIR)Personal/home/Secrets
+	@echo -e "\033[1;33mSyncing to NAS\033[0m"
+	rclone sync -v $(SYNC_SRC_DIR)Personal/home/Projects NAS:personal/Projects
