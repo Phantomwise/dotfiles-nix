fbba0ca9d6875597259cc29c8597ff693a05dbc7
Phantomwise <phantomwise67@protonmail.com>
2026-01-10 07:01:15 +0100

    ✨ Add packages for various games

--- nix/etc/nixos/packages-games.nix
+++ nix/etc/nixos/packages-games.nix
@@ -25,12 +25,15 @@
 		### GAMES ###
 		angband                  # Single-player roguelike dungeon exploration game
 		cataclysm-dda            # Free, post apocalyptic, zombie infested rogue-like
+		crossfire-client         # GTKv2 client for the Crossfire free MMORPG
 		exult                    # Recreation of Ultima VII for modern operating systems
+		flare                    # Fantasy action RPG using the FLARE engine
 		freedroidrpg             # Isometric 3D RPG similar to game Diablo
 		narsil                   # Unofficial rewrite of Sil, a roguelike influenced by Angband
 		nethack                  # Rogue-like game
 		nethack-qt               # Rogue-like game
 		unnethack                # Fork of NetHack
+		rpg-cli                  # Your filesystem as a dungeon
 		sil-q                    # Roguelike game set in the First Age of Middle-earth
 		tome2                    # Dungeon crawler similar to Angband, based on the works of Tolkien
 		unciv                    # Open-source Android/Desktop remake of Civ V
