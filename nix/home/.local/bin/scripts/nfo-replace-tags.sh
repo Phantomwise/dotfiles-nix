0e6254f098eb6be3ca570291177bfb59d1fb24d8
Phantomwise <phantomwise67@protonmail.com>
2026-01-30 03:17:57 +0100

    ✏️ Rename purple to magenta

--- nix/home/.local/bin/scripts/nfo-replace-tags.sh
+++ nix/home/.local/bin/scripts/nfo-replace-tags.sh
@@ -5,7 +5,7 @@ declare -r red="\033[0;31m"
 declare -r green="\033[0;32m"
 declare -r yellow="\033[0;33m"
 declare -r blue="\033[0;34m"
-declare -r purple="\033[0;35m"
+declare -r magenta="\033[0;35m"
 declare -r cyan="\033[0;36m"
 declare -r reset="\033[0m"
 
