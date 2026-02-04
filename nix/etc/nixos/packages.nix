3a7f296d9d1c8978890d5b256f79908df4264734
Phantomwise <phantomwise67@protonmail.com>
2026-02-04 11:41:36 +0100

    ✨ Add package for fzf

--- nix/etc/nixos/packages.nix
+++ nix/etc/nixos/packages.nix
@@ -121,6 +121,7 @@
 		### CLI ###
 		fastfetch                   # An actively maintained, feature-rich and performance oriented, neofetch like system information tool
 		figlet                      # Program for making large letters out of ordinary text
+		fzf                         # Command-line fuzzy finder written in Go
 		detox                       # Utility designed to clean up filenames
 
 		### NETWORKING ###
