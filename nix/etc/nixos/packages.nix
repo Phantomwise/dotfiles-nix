2d43c36cf822cf746dfcd607af314be9d9ef7767
Phantomwise <phantomwise67@protonmail.com>
2026-02-04 11:39:03 +0100

    ✨ Add some Haskell packages

--- nix/etc/nixos/packages.nix
+++ nix/etc/nixos/packages.nix
@@ -52,6 +52,8 @@
 		haskell-language-server     # LSP server for GHC
 		haskellPackages.Decimal     # Decimal numbers with variable precision
 		haskellPackages.hoogle      # Haskell API Search
+		haskellPackages.random      # Pseudo-random number generation
+		haskellPackages.yaml        # Support for parsing and rendering YAML documents
 			### OCAML ###
 		ocaml                       # OCaml is an industrial-strength programming language supporting functional, imperative and object-oriented styles
 		ocamlPackages.ocaml-lsp     # OCaml Language Server Protocol implementation
