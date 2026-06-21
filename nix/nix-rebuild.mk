925b927777d21fc76cbc0f41537787f65b787289
Phantomwise <phantomwise67@protonmail.com>
2026-06-21 18:38:41 +0200

    ✨ Add separate targets for laptop and desktop rebuilds/upgrade

--- nix/nix-rebuild.mk
+++ nix/nix-rebuild.mk
@@ -1,13 +1,20 @@
 # Declare phony targets
-.PHONY: nix-rebuild
+.PHONY: nix-rebuild nix-rebuild-laptop nix-rebuild-desktop
 
-# nix-rebuild
+NIXOS_CONFIG ?= /etc/nixos/configuration.nix
+
 nix-rebuild:
 	@echo -e "\033[1;33mRebuilding system configuration\033[0m"
-	sudo nixos-rebuild switch
+	sudo nixos-rebuild switch -I nixos-config=$(NIXOS_CONFIG)
 	@echo -e "\033[1;33mRebuilding Home Manager configuration\033[0m"
 	# home-manager switch
 	# @echo -e "\033[1;33mRefreshing font cache\033[0m"
 	fc-cache -fv
 	@echo -e "\033[1;32mRebuild complete\033[0m"
 	nvd diff $$(ls -d /nix/var/nix/profiles/system-*-link | tail -2)
+
+nix-rebuild-laptop:
+	$(MAKE) nix-rebuild NIXOS_CONFIG=/etc/nixos/configuration-laptop.nix
+
+nix-rebuild-desktop:
+	$(MAKE) nix-rebuild NIXOS_CONFIG=/etc/nixos/configuration-desktop.nix
--- nix/nix-update.mk
+++ nix/nix-update.mk
@@ -1,14 +1,21 @@
 # Declare phony targets
-.PHONY: nix-update
+.PHONY: nix-update nix-update-laptop nix-update-desktop
 
-# nix-update
+NIXOS_CONFIG ?= /etc/nixos/configuration.nix
+
 nix-update:
 	@echo -e "\033[1;33mUpdating channels\033[0m"
 	nix-channel --update
 	sudo nix-channel --update
 	@echo -e "\033[1;33mRebuilding\033[0m"
-	sudo nixos-rebuild switch --upgrade
+	sudo nixos-rebuild switch --upgrade -I nixos-config=$(NIXOS_CONFIG)
 	@echo -e "\033[1;33mRebuilding Home Manager configuration\033[0m"
 	# home-manager switch
 	# @echo -e "\033[1;32mUpdate complete\033[0m"
 	nvd diff $$(ls -d /nix/var/nix/profiles/system-*-link | tail -2)
+
+nix-update-laptop:
+	$(MAKE) nix-update NIXOS_CONFIG=/etc/nixos/configuration-laptop.nix
+
+nix-update-desktop:
+	$(MAKE) nix-update NIXOS_CONFIG=/etc/nixos/configuration-desktop.nix
