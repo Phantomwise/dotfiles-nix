b2165a50c4f7c2da860038bbabe5eb04b632d128
Phantomwise <phantomwise67@protonmail.com>
2026-06-21 18:25:22 +0200

    🎨 Move tlp to laptop config

--- nix/etc/nixos/configuration-laptop.nix
+++ nix/etc/nixos/configuration-laptop.nix
@@ -11,4 +11,13 @@
 			./packages-laptop.nix                   # Packages declarations
 		];
 
+	# Enable tlp for power management
+	services.tlp = {
+		enable = true;
+		settings = {
+				START_CHARGE_THRESH_BAT0 = 50;
+				STOP_CHARGE_THRESH_BAT0 = 80;
+		};
+	};
+
 }
