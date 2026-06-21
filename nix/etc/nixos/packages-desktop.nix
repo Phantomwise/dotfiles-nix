cfab45aac3a2363a9f2448877580c3908aaa6dbc
Phantomwise <phantomwise67@protonmail.com>
2026-06-21 18:26:06 +0200

    ✨ Add package for OBS

--- nix/etc/nixos/packages-desktop.nix
+++ nix/etc/nixos/packages-desktop.nix
@@ -5,6 +5,7 @@
 	environment.systemPackages = with pkgs; [
 
 		blender  # 3D Creation/Animation/Publishing System
+		obs-studio  # Free and open source software for video recording and live streaming
 
 	];
 
