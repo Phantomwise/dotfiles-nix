2021a0db7a1b36bb717d02eae6155af2bcacf6fd
Phantomwise <phantomwise67@protonmail.com>
2026-07-28 05:46:28 +0200

    ⏪ Comment out home manager

--- nix/nix-cleanup.mk
+++ nix/nix-cleanup.mk
@@ -14,15 +14,15 @@ nix-cleanup:
 	sudo nix-collect-garbage --delete-older-than 7d
 	@echo -e "\033[1;33mCleaning up unused user paths\033[0m"
 	nix-collect-garbage --delete-older-than 7d
-	@echo -e "\033[1;33mDeleting old home manager generations (keep last 7 days)\033[0m"
-	home-manager expire-generations "-7 days"
+	# @echo -e "\033[1;33mDeleting old home manager generations (keep last 7 days)\033[0m"
+	# home-manager expire-generations "-7 days"
 	@echo -e "\033[1;33mDeduplication\033[0m"
 	sudo nix-store --optimise
 	@echo -e "\033[1;33mListing remaining user generations\033[0m"
 	nix-env --list-generations
 	@echo -e "\033[1;33mListing remaining system generations\033[0m"
 	sudo nix-env --list-generations --profile /nix/var/nix/profiles/system
-	@echo -e "\033[1;33mList Home Manager generations\033[0m"
-	home-manager generations
+	# @echo -e "\033[1;33mList Home Manager generations\033[0m"
+	# home-manager generations
 	@echo -e "\033[1;33mList nvd diff\033[0m"
 	nvd diff $$(ls -d /nix/var/nix/profiles/system-*-link | tail -2)
