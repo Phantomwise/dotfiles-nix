f6bcc2ff799a76daa7ee8f8138698d9f718e131c
Phantomwise <phantomwise67@protonmail.com>
2026-07-28 05:48:20 +0200

    ✨ Add packages for Qt Creator and qttools

--- nix/etc/nixos/packages-desktop.nix
+++ nix/etc/nixos/packages-desktop.nix
@@ -7,6 +7,10 @@
 		blender  # 3D Creation/Animation/Publishing System
 		obs-studio  # Free and open source software for video recording and live streaming
 
+			### QT ###
+			qtcreator               # Cross-platform IDE tailored to the needs of Qt developers
+			qt6.qttools             # Cross-platform application framework for C++
+
 	];
 
 }
