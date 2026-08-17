77ce88268e09c57f623457bee812b0a3ce93d304
Phantomwise <phantomwise67@protonmail.com>
2026-08-17 03:49:59 +0200

    🚸 Add sound notification

--- nix/home/.local/bin/scripts/is-internet-back-already.sh
+++ nix/home/.local/bin/scripts/is-internet-back-already.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 while true; do
-    ping -c 1 -W 2 8.8.8.8 > /dev/null 2>&1 && notify-send "Yay internet is back \o/"
+    ping -c 1 -W 2 8.8.8.8 > /dev/null 2>&1 && "$HOME/.local/bin/scripts/dunst/play-sound-connect.sh" && notify-send "Yay internet is back \o/"
     sleep 15
 done
