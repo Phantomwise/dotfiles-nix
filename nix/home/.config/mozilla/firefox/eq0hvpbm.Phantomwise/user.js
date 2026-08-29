4c565a0e1f6542f2ff051324c4fd737677b2679d
Phantomwise <phantomwise67@protonmail.com>
2026-08-29 15:15:18 +0200

    🔧 Add `user.js` for firefox config

---
+++ nix/home/.config/mozilla/firefox/eq0hvpbm.Phantomwise/user.js
@@ -0,0 +1,28 @@
+// Telemetry
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+
+// Annoyances
+user_pref("signon.rememberSignons", false);
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+
+// Behavior
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.startup.homepage", "about:blank");
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.startup.page", 3);
+
+// UI
+user_pref("browser.uidensity", 0);
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+
+// AI
+user_pref("browser.ai.control.default", "blocked");
+user_pref("browser.ai.control.linkPreviewKeyPoints", "blocked");
+user_pref("browser.ai.control.pdfjsAltText", "blocked");
+user_pref("browser.ai.control.sidebarChatbot", "blocked");
+user_pref("browser.ai.control.smartTabGroups", "blocked");
+user_pref("browser.ai.control.smartWindow", "blocked");
+user_pref("browser.ai.control.translations", "default");
