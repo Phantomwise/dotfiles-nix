708f3cea7b44b5aaef1ec824e29bb407276d47f0
Phantomwise <phantomwise67@protonmail.com>
2026-08-31 17:19:12 +0200

    ⏪ Go back to the gtk file picker

--- nix/etc/nixos/wayland.nix
+++ nix/etc/nixos/wayland.nix
@@ -6,16 +6,17 @@
 		enable = true;
 		wlr.enable = true;
 		# gtk portal needed to make firefox happy
-		# extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-		extraPortals = [ pkgs.xdg-desktop-portal-gtk pkgs.kdePackages.xdg-desktop-portal-kde ];
-		config = {
-			common = {
-				default = [ "gtk" ];
-			};
-				sway = {
-					"org.freedesktop.impl.portal.FileChooser" = [ "kde" ];
-			};
-		};
+		extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+		# extraPortals = [ pkgs.xdg-desktop-portal-gtk pkgs.kdePackages.xdg-desktop-portal-kde ];
+		# config = {
+		# 	common = {
+		# 		default = [ "gtk" ];
+		# 	};
+		# 		sway = {
+		# 			"org.freedesktop.impl.portal.FileChooser" = [ "kde" ];
+					# Doesn't have gvfs network discovery...
+		# 	};
+		# };
 		# gtkUsePortal = true;
 			# error:
 			# The option definition `xdg.portal.gtkUsePortal' in `/home/phantomwise/SynologyDrive/dotfiles-nix/nix/etc/nixos/configuration.nix' no longer has any effect; please remove it.
